222aee198732e6faef7be6ae2c719cbf34e85e1e
rayizzmusa <127111995+rayizzmusa@users.noreply.github.com>
2023-10-12 19:52:27 +0700

    Add files via upload

---
+++ LAT 2/lat2.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonDaftar = document.querySelector(".custom-btn");
+
+  buttonDaftar.addEventListener("click", function () {
+    const nama = document.getElementById("input1").value;
+    const email = document.getElementById("input2").value;
+    const alamat = document.getElementById("input3").value;
+
+    if (nama === "" || email === "" || alamat === "") {
+      // Menggunakan operator logika '||' untuk menggabungkan kondisi
+      alert("Data Gaboleh Kosong!");
+    } else {
+      alert("Pendaftaran Sukses!");
+    }
+  });
+});
